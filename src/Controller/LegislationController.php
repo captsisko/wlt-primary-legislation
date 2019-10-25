d1202ad69c607fba44cc4488172752cdca341047
Sisko <adeoduye@gmail.com>
2019-10-25 15:51:31 +0000

    Initial xml fetching proof of concept

--- src/Controller/LegislationController.php
+++ src/Controller/LegislationController.php
@@ -4,6 +4,11 @@ use Drupal\Core\Controller\ControllerBase;
 
 class LegislationController extends ControllerBase{
 	public function content(){
+		$xml = \simplexml_load_file('http://www.legislation.gov.uk/primary/data.feed');
+		$size = \sizeof($xml->entry);
+		drupal_set_message("Detecting {$size} entries");
+		dsm($xml);
+		
 		$build = [
 			'#markup'=> $this->t('Whaz upppppppp!'),
 		];
